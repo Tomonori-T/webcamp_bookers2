f1363c68835ba71884383f12a467ad9fcd4156bb
Tomonori-T <noritt3397@gmail.com>
2021-04-28 13:55:36 +0000

    [error] 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top, :about]
 
   protected
 
@@ -7,7 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
-
+  
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
